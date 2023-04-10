9029d3995214c4b72cbb5a19a0517fca265af694
Welnic <catalinamelnic@gmail.com>
2023-04-10 18:48:44 +0300

    Added input box for API key

---
+++ README.md
@@ -0,0 +1,29 @@
+# ChatGPT Weather Anchor
+
+## About
+
+A Streamlit app powered by ChatGPT that lets you chat with a weather anchor using GPT-3.5 and weatherapi.com
+
+## Features
+
+- ❤️ Powered by ChatGPT
+- ⭐️ Minimalist Streamlit UI
+- 🪶 Ask about the weather
+- ⚡️ Free & open-source
+
+## See also
+
+- [ChatGPT](https://platform.openai.com/docs/api-reference/completions)
+- [`streamlit`](https://docs.streamlit.io/)
+
+
+## Development guide
+
+_This app is built with [Streamlit](https://docs.streamlit.io/)._
+
+### Pre-requisites
+
+- [Python](https://www.python.org/)
+- [Poetry](https://python-poetry.org/)
+- [WeatherAPI Key](www.weatherapi.com)
+- [OpenAI API Key](https://platform.openai.com/docs/api-reference/completions)
--- app.py
+++ app.py
@@ -6,8 +6,11 @@ import requests
 import json
 load_dotenv()
 
-openai.api_key = os.getenv("OPENAI_API_KEY")
 weather_api_key = os.getenv("WEATHER_API")
+
+def add_openai_api_key():
+  openai.api_key = st.session_state.openai_api_key
+
 
 def extract_city():
     system_message = {"role" : "system", "content" : "You are a helpful text parser."}
@@ -29,40 +32,58 @@ def extract_weather_data_json(city):
   return data
 
 def get_weather_data(json_data):
-  assistant_message = {"role": "system", "content": "You are a helpful json interpreter"}
-  completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[assistant_message, {
+  system_message = {"role": "system", "content": "You are a helpful json interpreter"}
+  completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[system_message, {
     "role": "user",
     "content": 'Get current weather insights from the following data : ' + json_data}])
   weather_insights = completion.choices[0].message.content
   return weather_insights
 
 def on_message_change():
-    st.session_state.history.append({"role": "user" , "content": st.session_state.message})
-    city = extract_city()
-    json_data = extract_weather_data_json(city)
-    weather_insights = get_weather_data(json.dumps(json_data))
-    st.session_state.history.append({"role" : "assistant", "content" : weather_insights})
-    st.session_state.message = ""
+    if st.session_state.openai_api_key == "":
+      st.error("Please add your Openai API Key", icon="🚨")
+    else:
+      try:
+        st.session_state.history.append({"role": "user" , "content": st.session_state.message})
+        city = extract_city()
+        json_data = extract_weather_data_json(city)
+        weather_insights = get_weather_data(json.dumps(json_data))
+        st.session_state.history.append({"role" : "assistant", "content" : weather_insights})
+        st.session_state.message = ""
+      except openai.error.AuthenticationError:
+        st.error("Your API key or token is invalid, expired, or revoked. Check again and update your API key", icon="🚨")
 
 
 def main():
-
+  st.set_page_config(
+    page_title="ChatGPT Weather Anchor",
+    page_icon="🍃",
+    menu_items={
+        'Get Help': 'https://platform.openai.com/account/api-keys',
+        'Report a bug': "https://www.extremelycoolapp.com/bug",
+        'About': """#### Streamlit weather app powered by ChatGPT. *Ask ChatGPT how is the weather today in your location.*
+                  Author: https://bento.me/catalina"""
+    }
+  )    
   st.title("Get weather insights with ChatGPT")
-  assistant_message = {"role" : "assistant", "content" : "You are a weather anchor."}
+  system_message = {"role" : "system", "content" : "You are a weather anchor."}
 
   if "history" not in st.session_state:
-      st.session_state.history = [assistant_message]
+      st.session_state.history = [system_message]
+
+  if "openai_api_key" not in st.session_state:
+      st.session_state.openai_api_key = ""
 
   for message in st.session_state.history:
       if message["role"] == "user":
-        st.write('<p style="color:orchid;text-align:right;">' + "User: " + message["content"] + '</p>', unsafe_allow_html=True)
+        st.write('<p style="color:limegreen;text-align:right;">' + "User: " + message["content"] + '</p>', unsafe_allow_html=True)
       elif message["role"] == "assistant":
         st.write("ChatGPT: " + message["content"])
       else:
         st.write("System: " + message["content"])
          
 
-  st.text_input(label="Message", key="message", placeholder = 'Enter your message here', on_change=on_message_change, label_visibility="hidden")
-
+  st.text_input(label="Message", key="message", placeholder = 'Ask ChatGPT how is the weather today in your location.', on_change=on_message_change, label_visibility="hidden")
+  st.text_input(label="openai_api_key", key = "openai_api_key", type = "password", placeholder = 'Add your Openai API Key here', on_change=add_openai_api_key, label_visibility="hidden")
 
 main()
